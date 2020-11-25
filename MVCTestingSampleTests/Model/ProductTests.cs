bb08b10d5370f9948740080a38dfeef8f7a485bf
Obe <o.bdondo@gmail.com>
2020-11-24 18:18:50 -0800

    Added property test and static method testd

---
+++ MVCTestingSampleTests/Model/ProductTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCTestingSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCTestingSampleTests.Model
+{
+    [TestClass]
+    
+    public class ProductTests
+    {
+        [TestMethod]
+        public void Name_SetToNull_ThrowsArguementNullException()
+        {
+            Product p = new Product();
+            Assert.ThrowsException<ArgumentNullException>
+                (
+                    () => p.Name = null
+                );
+        }
+    }
+}
---
+++ MVCTestingSampleTests/Models/ValidationHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCTestingSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCTestingSample.Models.Tests
+{
+    [TestClass()]
+    public class ValidationHelperTests
+    {
+        [TestMethod()]
+        [DataRow("9.99")]
+        [DataRow("$20.00")] ///works with US currency only
+        [DataRow(".99")]
+        [DataRow("0")]
+        [DataRow("100000000")]
+        public void IsValidPriceTest_ValidPrice_ReturnsTrue(string input)
+        {
+            bool result = ValidationHelper.IsValidPrice(input);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow("")]
+        [DataRow("  ")]
+        [DataRow("$5.00.01")]
+
+        public void IsValidPriceTest_ValidPrice_ReturnsFalse(string input)
+        {
+            bool result = ValidationHelper.IsValidPrice(input);
+            Assert.IsFalse(result);
+        }
+    }
+}
